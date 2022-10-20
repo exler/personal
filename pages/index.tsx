46ded5a87a1a2fd073584fd49ee7645815788c9c
Kamil Marut <kamil@kamilmarut.com>
2022-10-21 00:40:22 +0200

    feat(document): Add title and meta description

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from 'next'
+import Head from 'next/head'
 import Image from 'next/image'
 import Projects from '../components/Projects'
 import Socials from '../components/Socials'
@@ -7,6 +8,10 @@ import styles from '../styles/Home.module.css'
 const Home: NextPage = () => {
   return (
     <>
+      <Head>
+        <title>Kamil Marut</title>
+        <meta name="description" content="Software engineer specializing in development of high-performance and maintainable digital products, with expertise in pragmatic system design and rapid prototyping." />
+      </Head>
       <div>
         <h3>Hi, I&apos;m Kamil.</h3>
         <div className={styles.side_by_side}>
