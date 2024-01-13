7f59189533c8e0242970361a6cb595d32a3ca515
Kamil Marut <kamil@kamilmarut.com>
2024-01-13 09:38:03 +0100

    chore(deps): Add Prettier

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,27 +3,27 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	darkMode: 'class',
-	theme: {
-		extend: {
-			colors: {
-				gray: colors.neutral,
-				background: "#1a1a1a",
-				primary: "#ed9d00",
-				secondary: "#fece2f"
-			},
-			fontFamily: {
-				sans: ["Work Sans", ...defaultTheme.fontFamily.sans],
-				stock: [defaultTheme.fontFamily.sans]
-			},
-			aspectRatio: {
-				"4/3": "4 / 3",
-				"3/2": "3 / 2",
-				"2/3": "2 / 3",
-				"9/16": "9 / 16"
-			}
-		}
-	},
-	plugins: [],
-}
+    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+    darkMode: "class",
+    theme: {
+        extend: {
+            colors: {
+                gray: colors.neutral,
+                background: "#1a1a1a",
+                primary: "#ed9d00",
+                secondary: "#fece2f"
+            },
+            fontFamily: {
+                sans: ["Work Sans", ...defaultTheme.fontFamily.sans],
+                stock: [defaultTheme.fontFamily.sans]
+            },
+            aspectRatio: {
+                "4/3": "4 / 3",
+                "3/2": "3 / 2",
+                "2/3": "2 / 3",
+                "9/16": "9 / 16"
+            }
+        }
+    },
+    plugins: []
+};
