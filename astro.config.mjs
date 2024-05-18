b572910730aa04145c449c11cda645145359830f
Kamil Marut <kamil@kamilmarut.com>
2024-05-18 08:42:01 +0200

    feat: Prefetch pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
     site: "https://kamilmarut.com",
-    integrations: [tailwind()]
+    integrations: [tailwind()],
+    prefetch: true
 });
